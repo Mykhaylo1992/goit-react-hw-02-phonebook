c9c459c1de46092640b81c1af3df91247d7ee6c0
Mykhaylo1992 <misha.dyatel@gmail.com>
2020-05-14 14:20:22 +0300

    Updates

---
+++ precache-manifest.a0a6dced4c0e26789fb387624d26cc39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d955a07fa4d72ad01baf3976f94af6",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "62e6800b0156bbc79e23",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.e67f2da9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.e67f2da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "febaea54f29f6e91b0c6",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.c243bf6c.chunk.js"
+  },
+  {
+    "revision": "c73043600cf8c264972f",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.2d9dc3c9.js"
+  }
+]);
